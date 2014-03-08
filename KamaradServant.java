c22c7577a9636dad7c5fc6e54c43c16249f2f240
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-03-08 13:26:15 +0000

    Implements the register method

--- KamaradServant.java
+++ KamaradServant.java
@@ -1,15 +1,25 @@
 import org.omg.PortableServer.*;
 import Kamarad.*;
 import org.omg.CORBA.*;
+import java.util.*;
 
 class KamaradServant implements KamaradOnlineOperations {
 
+	private Hashtable<String, KamaradAccountDetails> allAccounts = new Hashtable<String, KamaradAccountDetails>();
 	private KamaradAccountDetails accountDetails;
-
 	private static long ID = 0001;
 
 	public void register (org.omg.CORBA.Any kamaradAccountDetails, org.omg.CORBA.AnyHolder uniqueId) {
-		
+		String strLong = String.format("%04d" , this.ID++);
+
+		accountDetails = KamaradAccountDetailsHelper.extract(kamaradAccountDetails);
+		accountDetails.unique_id = strLong;
+		allAccounts.put(strLong, accountDetails);
+
+		Any keyAux = ORB.init().create_any();
+		keyAux.insert_string(strLong);
+		uniqueId.value = keyAux;
+
 	}
 
   public void topup (org.omg.CORBA.Any uniqueId, org.omg.CORBA.Any amount, org.omg.CORBA.AnyHolder kamaradAccountDetails) {
