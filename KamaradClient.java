f9c98a246e497f5ecdfa4d7d35eb03416a8e7705
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-03-09 18:27:20 +0000

    Implements the client

--- KamaradClient.java
+++ KamaradClient.java
@@ -12,6 +12,8 @@ import org.omg.CORBA.ORBPackage.InvalidName;
 
 public class KamaradClient {
   public static NamingContextExt rootCtx;
+  private static ORB orb;
+  private static KamaradOnline kamaradRef;
 
   public static void main(String args[]) {
 		try {
@@ -21,7 +23,7 @@ public class KamaradClient {
 			prop.put("org.omg.CORBA.ORBInitialPort", "1050");
 
 			// create and initialize the ORB
-			ORB orb = ORB.init(args, prop);
+			orb = ORB.init(args, prop);
 			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
 			rootCtx = NamingContextExtHelper.narrow(objRef);
 
@@ -30,67 +32,145 @@ public class KamaradClient {
 
 			// NameComponent path[] = {nc};
 			org.omg.CORBA.Object objRefKamarad = rootCtx.resolve(nc);
-			KamaradOnline kamaradRef = KamaradOnlineHelper.narrow(objRefKamarad);
+			kamaradRef = KamaradOnlineHelper.narrow(objRefKamarad);
 
-			// Stub to call REGISTER mehtod #########################################
-			float balance = Float.parseFloat("250");
-			float kamaradCredit = Float.parseFloat("0");
+			Integer option = 0;
 
-			KamaradAccountDetails accountDetails = new KamaradAccountDetails(
-					"", // uniqueId
-					"Alan Turing", // name
-					"0831776655", // phoneNumber
-					"NCI on Campus", // address
-					9876543, // bankNumber
-					balance, // balance
-					kamaradCredit // kamaradCredit
-			);
+			while	(option != 4) {
+				// Display menu
+		    System.out.println("============================");
+		    System.out.println("|      MENU SELECTION      |");
+		    System.out.println("============================");
+		    System.out.println("| Options:                 |");
+		    System.out.println("|        1. Register       |");
+		    System.out.println("|        2. Top up         |");
+		    System.out.println("|        3. Get Credit     |");
+		    System.out.println("|        4. Exit           |");
+		    System.out.println("============================");
 
-			Any anyAccount = orb.create_any();
-			AnyHolder uniqueId = new AnyHolder();
-			
-			try {
-				KamaradAccountDetailsHelper.insert(anyAccount, accountDetails);
-			} catch (SystemException se) {
-				System.out.println("insert any error\n" + "Unexpected exception:\n" + se.toString ());
-				return;
+		    System.out.println("Select option:");
+		    option = Integer.parseInt(System.console().readLine());
+
+				// Switch construct
+				switch (option) {
+					case 1:
+						String resp1 = register();
+						System.out.println(resp1);
+						System.out.println("Enter to continue.");
+						System.console().readLine();
+						break;
+					case 2:
+						KamaradAccountDetails resp2 = topup();
+						System.out.println(resp2.balance);
+						System.out.println("Enter to continue.");
+						System.console().readLine();
+						break;
+					case 3:
+						Float resp3 = getCredit();
+						System.out.println(resp3);
+						System.out.println("Enter to continue.");
+						System.console().readLine();
+						break;
+					case 4:
+						System.out.println("bye...");
+						break;
+					default:
+						System.out.println("Invalid Option");
+						break;
+				}
+
 			}
-
-			kamaradRef.register(anyAccount, uniqueId);
-			System.out.println(uniqueId.value.extract_string());
-
-			
-			// Stub to call TOPUP mehtod ############################################
-			Any anyID = orb.create_any();
-			anyID.insert_string("0001");
-
-			Any anyAmount = orb.create_any();
-			anyAmount.insert_float(15);
-
-			AnyHolder anyHolderAccount = new AnyHolder();
-
-			kamaradRef.topup(anyID, anyAmount, anyHolderAccount);
-			accountDetails = KamaradAccountDetailsHelper.extract(anyHolderAccount.value);
-			System.out.println(accountDetails.unique_id);
-			System.out.println(accountDetails.name);
-			System.out.println(accountDetails.phone_number);
-			System.out.println(accountDetails.address);	
-			System.out.println(accountDetails.bank_number);
-			System.out.println(accountDetails.balance);
-			System.out.println(accountDetails.kamarad_credit);
-
-
-
-			// Stub to call GETCREDIT mehtod ############################################
-			AnyHolder anyHolderCredit = new AnyHolder();
-
-			kamaradRef.getCredit(anyID, anyHolderCredit);
-			System.out.println(anyHolderCredit.value.extract_float());
-
 
 		} catch (Exception e) {
 			System.out.println("ERROR : " + e) ;
 			e.printStackTrace(System.out);
 		}
 	}
+
+
+	private static String register() {
+		System.out.print("Enter you name: ");
+		String name = System.console().readLine();
+
+		System.out.print("Enter you phone_number: ");
+		String phone_number = System.console().readLine();
+
+		System.out.print("Enter you address: ");
+		String address = System.console().readLine();
+
+		System.out.print("Enter you bank_number: ");
+		Integer bank_number = Integer.parseInt(System.console().readLine());
+
+		System.out.print("Enter you balance: ");
+		Long balance = Long.parseLong(System.console().readLine());
+
+		float kamarad_credit = Float.parseFloat("0");
+
+		KamaradAccountDetails accountDetails = new KamaradAccountDetails(
+				"", // unique_id
+				name, // name
+				phone_number, // phone_number
+				address, // address
+				bank_number, // bank_number
+				balance, // balance
+				kamarad_credit // kamarad_credit
+		);
+
+		Any anyAccount = orb.create_any();
+		AnyHolder uniqueId = new AnyHolder();
+		
+		try {
+			KamaradAccountDetailsHelper.insert(anyAccount, accountDetails);
+		} catch (SystemException se) {
+			System.out.println("insert any error\n" + "Unexpected exception:\n" + se.toString ());
+			return "error";
+		}
+
+		kamaradRef.register(anyAccount, uniqueId);
+		return uniqueId.value.extract_string();
+	}
+
+
+	private static KamaradAccountDetails topup() {
+		System.out.print("Password: ");
+		String password = System.console().readLine();
+
+		System.out.print("Amount: ");
+		Long amount = Long.parseLong(System.console().readLine());
+
+		Any anyID = orb.create_any();
+		anyID.insert_string(password);
+
+		Any anyAmount = orb.create_any();
+		anyAmount.insert_float(amount);
+
+		AnyHolder anyHolderAccount = new AnyHolder();
+
+		kamaradRef.topup(anyID, anyAmount, anyHolderAccount);
+		KamaradAccountDetails accountDetails = KamaradAccountDetailsHelper.extract(anyHolderAccount.value);
+
+		// System.out.println(accountDetails.unique_id);
+		// System.out.println(accountDetails.name);
+		// System.out.println(accountDetails.phone_number);
+		// System.out.println(accountDetails.address);	
+		// System.out.println(accountDetails.bank_number);
+		// System.out.println(accountDetails.balance);
+		// System.out.println(accountDetails.kamarad_credit);
+
+		return accountDetails;
+	}
+
+
+	private static Float getCredit() {
+		System.out.print("Password: ");
+		String password = System.console().readLine();
+
+		Any anyID = orb.create_any();
+		anyID.insert_string(password);
+		AnyHolder anyHolderCredit = new AnyHolder();
+
+		kamaradRef.getCredit(anyID, anyHolderCredit);
+		return anyHolderCredit.value.extract_float();
+	}
+
 }
